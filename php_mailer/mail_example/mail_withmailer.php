cd7aa6ff2fdbafe5452b4f0eb1736975bd84b466
David Goodman <deg5112@gmail.com>
2016-01-07 14:43:13 -0800

    fixed css

--- php_mailer/mail_example/mail_withmailer.php
+++ php_mailer/mail_example/mail_withmailer.php
@@ -4,6 +4,9 @@ require('phpmailer/PHPMailer/PHPMailerAutoload.php');
 $name = $_POST['name'];
 $email = $_POST['email'];
 $message = $_POST['message'];
+$phone1 = $_POST['phone1'];
+$phone2 = $_POST['phone2'];
+$phone3 = $_POST['phone3'];
 
 $mail = new PHPMailer;
 $mail->SMTPDebug = 3;                               // Enable verbose debug output
@@ -27,22 +30,21 @@ $options = array(
 $mail->smtpConnect($options);
 $mail->From = 'davidgoodmandeveloper@gmail.com';
 $mail->FromName = 'David Goodman';
-$mail->addAddress('davidgoodmandeveloper@gmail.com', 'David');     // Add a recipient
+$mail->addAddress($email, $name);     // Add a recipient
 //$mail->addAddress('ellen@example.com');               // Name is optional
-$mail->addReplyTo($email, $name);
+$mail->addReplyTo('davidgoodmandeveloper@gmail.com', 'David');
 //$mail->addCC('cc@example.com');
-//$mail->addBCC('bcc@example.com');
-
-//$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+$mail->addBCC('davidgoodmandeveloper@gmail.com');
+$mail->addAttachment('../../-The David Eric Goodman Resume-.doc');         // Add attachments
 //$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
 $mail->isHTML(true);
-$link1 = "<a href='https://www.linkedin.com/profile/view?id=AAIAAAVkYgIBqxwIqudQ6kJ2xpG6hcBNFVp0b8M&trk=nav_responsive_tab_profile'><img style='width: 50px; height: 50px' src='https://upload.wikimedia.org/wikipedia/commons/thumb/e/e9/Linkedin_icon.svg/2000px-Linkedin_icon.svg.png'></a>";
-$link2 = "<a href='https://github.com/deg5112'><img style='width: 100px; height: 50px' src='http://www.aha.io/assets/integration_logos/github-bb449e0ffbacbcb7f9c703db85b1cf0b.png'></a>";
-
+$link1 = "<a href='https://www.linkedin.com/profile/view?id=AAIAAAVkYgIBqxwIqudQ6kJ2xpG6hcBNFVp0b8M&trk=nav_responsive_tab_profile'><img style='width: 4.5vw; height: 6vh' src='https://upload.wikimedia.org/wikipedia/commons/thumb/e/e9/Linkedin_icon.svg/2000px-Linkedin_icon.svg.png'></a>";
+$link2 = "<a href='https://github.com/deg5112'><img style='width: 10vw; height: 7vh' src='http://www.aha.io/assets/integration_logos/github-bb449e0ffbacbcb7f9c703db85b1cf0b.png'></a>";
 $mail->Subject = 'Thank you for reaching out - David Goodman';
 $mail->Body    =
 "<pre style='font-family:times; font-size: 16px'>
-<b>Message</b>: '$message'
+<b>Your Message:</b> '$message'
+<p>Phone:</p>
 
 Hi $name,
 
@@ -50,7 +52,10 @@ Thanks so much for getting in touch, I will respond to you as soon as possible,
 
 David Goodman
 Web Developer
-$link1  $link2
+davidgoodmandeveloper@gmail.com
+952-693-3823
+
+$link1 $link2
 <pre>";
 
 $mail->AltBody = $message;
