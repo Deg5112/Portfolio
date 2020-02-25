c19d8c0cb32a0da6f6b295c2e9911b86f9e20d9f
davidgoodman <deg5112@gmail.com>
2020-02-25 02:16:23 -0800

    add checkers block

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var path = require('path');
 
 app.use(express.static(path.join(__dirname)));
 app.use("/styles", express.static(__dirname));
+app.use("/css", express.static(__dirname + '/css'));
 app.use("/images", express.static(__dirname + '/images'));
 app.use("/scripts", express.static(__dirname + '/scripts'));
 
